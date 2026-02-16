18623e48bd0d9d68b302b91e16491672eff65ead
Matheus <mps.web.tech@gmail.com>
2026-02-16 20:05:10 -0300

    feat: create webhook schema

---
+++ api/src/db/schema/webhooks.ts
@@ -0,0 +1,26 @@
+import { pgTable, text, integer, jsonb, timestamp } from "drizzle-orm/pg-core";
+import { uuidv7 } from "uuidv7";
+
+export const webhooks = pgTable("webhooks", {
+  id: text()
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
+
+  method: text().notNull(),
+
+  pathname: text().notNull(),
+
+  ip: text().notNull(),
+
+  contentType: text(),
+
+  contentLength: integer(),
+
+  queryParams: jsonb().$type<Record<string, string>>(),
+
+  headers: jsonb().$type<Record<string, string>>().notNull(),
+
+  body: text(),
+
+  createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+});
