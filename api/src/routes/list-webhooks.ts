bd6ae4987516c9e6f9cfffa840176bac35a7c2d5
Matheus <mps.web.tech@gmail.com>
2026-02-17 15:34:36 -0300

    chore: update route list-webhook

--- api/src/routes/list-webhooks.ts
+++ api/src/routes/list-webhooks.ts
@@ -1,5 +1,9 @@
-import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
-import z, { string } from "zod";
+import { webhooks } from "@/db/schema"
+import { createSelectSchema } from "drizzle-zod"
+import { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import { db } from "@/db"
+import { desc, lt } from "drizzle-orm"
+import { z } from "zod"
 
 export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
   app.get(
@@ -10,20 +14,50 @@ export const listWebhooks: FastifyPluginAsyncZod = async (app) => {
         tags: ["Webhooks"],
         querystring: z.object({
           limit: z.coerce.number().min(1).max(100).default(20),
+          cursor: z.string().optional(),
         }),
         response: {
-            200:z.array(
-                z.object({
-                    id:z.string(),
-                    method: z.string()
-                })
-            )
-        }
+          200: z.object({
+            webhooks: z.array(
+              createSelectSchema(webhooks).pick({
+                id: true,
+                method: true,
+                pathname: true,
+                createdAt: true,
+              }),
+            ),
+            nextCursor: z.string().nullable(),
+          }),
+        },
       },
     },
-    async (request, reply) => {
-      const { limit } = request.query;
-      return [];
+    async (request) => {
+      const { limit, cursor } = request.query
+
+      const results = await db
+        .select({
+          id: webhooks.id,
+          method: webhooks.method,
+          pathname: webhooks.pathname,
+          createdAt: webhooks.createdAt,
+        })
+        .from(webhooks)
+        .where(cursor ? lt(webhooks.id, cursor) : undefined)
+        .orderBy(desc(webhooks.id))
+        .limit(limit + 1)
+
+      const hasNextPage = results.length > limit
+
+      const items = hasNextPage ? results.slice(0, -1) : results
+
+      const nextCursor = hasNextPage
+        ? items[items.length - 1].id
+        : null
+
+      return {
+        webhooks: items,
+        nextCursor,
+      }
     },
-  );
-};
+  )
+}
